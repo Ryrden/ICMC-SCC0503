c042ed4f92f734939615693e0ad87112833cca6a
Ryan Souza <ryansateles@gmail.com>
2022-03-23 14:46:23 -0300

    estrutura base

---
+++ Exercice5/Util/util.c
@@ -0,0 +1,45 @@
+#include "util.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void boolean_print(boolean bool) {
+    if (bool == TRUE) {
+        printf("TRUE\n");
+    } else if (bool == FALSE) {
+        printf("FALSE\n");
+    } else {
+        printf("ERRO\n");
+    }
+}
+
+char *readLine() {
+    char *string = NULL;
+    char currentInput;
+    int index = 0;
+    do {
+        currentInput = (char)getchar();
+        string = (char *)realloc(string, sizeof(char) * (index + 1));
+        string[index] = currentInput;
+        index++;
+        if (currentInput == '\r') {
+            currentInput = (char)getchar();
+        }
+    } while ((currentInput != '\n') && (currentInput != EOF));
+    string[index - 1] = '\0';
+    return string;
+}
+
+char *readStringUntilReach(char caractere) {
+    char c;
+    char *string = (char *)malloc(sizeof(char) * 256);
+    int index = 0;
+    while (scanf("%c", &c) != EOF) {
+        if (c == caractere) {
+            string[index] = '\0';
+            break;
+        }
+        string[index] = c;
+        index++;
+    }
+    return string;
+}
---
+++ Exercice7/Util/util.h
@@ -0,0 +1,16 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#define TRUE 1
+#define FALSE 0
+#define ERROR -32000
+
+typedef int boolean;
+
+void boolean_print(boolean bool);
+
+char *readStringUntilReach(char caractere);
+
+char *readLine();
+
+#endif // UTIL_H
