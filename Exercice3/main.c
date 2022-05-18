6953259ac332ba00e79373fbc8fb86911ddc2083
Ryan <ryansateles@gmail.com>
2022-05-17 23:12:26 -0300

    logica da main

--- Exercice3/main.c
+++ Exercice3/main.c
@@ -1,5 +1,6 @@
-#include "./pageHandler/pageHandler.h"
 #include "./bTree/bTree.h"
+#include "./uspDatabase/uspDatabase.h"
+#include "./pageHandler/pageHandler.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,6 +17,54 @@
 */
 
 int main() {
+    FILE *dataFile = fopen("datafile.bin", "wb+");
+    verifyNullPointerExceptionToFile(dataFile);
 
+    int RRN = 0;
+    long studentSize = get_student_data_size();
+    char *line = (char *)malloc(sizeof(char) * STRING_SIZE);
+    while (fgets(line, sizeof(char) * STRING_SIZE, stdin)) {
+        char *token = strtok(line, " ");
+        char *command = token;
+
+        if (select_command(command) == insert_) {
+            NUSP nusp;
+            NAME name;
+            LASTNAME lastName;
+            COURSE course;
+            GRADE grade;
+
+            token = strtok(NULL, ",");
+            nusp = atoi(token);
+
+            token = strtok(NULL, ",");
+            strcpy(name, token);
+
+            token = strtok(NULL, ",");
+            strcpy(lastName, token);
+
+            token = strtok(NULL, ",");
+            strcpy(course, token);
+
+            token = strtok(NULL, ",");
+            grade = atof(token);
+
+            STUDENT *student = create_student(nusp,name,lastName,course,grade);
+            
+        } else if (select_command(command) == search_) {
+            token = strtok(NULL, ",");
+            unsigned int key = atoi(token);
+
+        } else if (select_command(command) == delete_) {
+            token = strtok(NULL, ",");
+            unsigned int key = atoi(token);
+
+        } else if (select_command(command) == exit_) {
+            break;
+        }
+    }
+    free(line);
+    fclose(dataFile);
+    fclose(indexFile);
     return EXIT_SUCCESS;
 }
