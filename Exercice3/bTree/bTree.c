a027382c89f85834deeca46191e25cf6686f24c7
silmarp <silmarjr2@gmail.com>
2022-05-17 22:54:23 -0300

    done create functions

--- Exercice3/bTree/bTree.c
+++ Exercice3/bTree/bTree.c
@@ -24,14 +24,14 @@ struct promotedKey_st {
 };
 
 /*Retrives page from file pointer*/
-btPage *
+BTPAGE *
 readPageFromFile(FILE *fp) {
     // Aloca espaço para carregar página
     // Lê dados da página do arquivo
 }
 
 /*Writes page into file in certain rrn position*/
-Errors writePageIntoFile(long rrn, btPage *page, FILE *fp) {
+Errors writePageIntoFile(long rrn, BTPAGE *page, FILE *fp) {
     // Verifica se está tudo ok com os dados
     // Encontra local para escrita baseado no RRN
     // Escreve dados
@@ -40,7 +40,7 @@ Errors writePageIntoFile(long rrn, btPage *page, FILE *fp) {
 }
 
 /*Get page by rrn*/
-btPage *getPage(long RRN, FILE *fp) {
+BTPAGE *getPage(long RRN, FILE *fp) {
     // Recupera uma página baseado no RRN
     // Procura e carrega seus dados
 }
@@ -55,27 +55,27 @@ void writeTreeHeader(FILE *fp, long rootRRN) {
     // Calcula espaço livre e escreve no cabeçalho da árvore, junto com o RRN do nó raíz
 }
 
-btPage *createTree(FILE *fp) {
+BTPAGE *createTree(FILE *fp) {
     // Aloca espaço pra raiz
     // Inicializa os valores
     // Escreve a raiz no cabeçalho
 }
 
-btPage *getOrCreateRoot(FILE *fp) {
+BTPAGE *getOrCreateRoot(FILE *fp) {
     // Verifica se a árvore já existe ou precisa criar uma nova
     // Se a árvore não existir, cria ela
     // Se existir, só pega o RRN da raiz no cabeçalho e carrega sua página
     // Pode ser adaptada pra inserção e busca sem precisar de 2 funções
 }
 
-promotedKey *insertIntoNode(btPage *page, promotedKey *newKey, FILE *fp) {
+promotedKey *insertIntoNode(BTPAGE *page, promotedKey *newKey, FILE *fp) {
     // Procura local pra inserir nova chave na página
     // Se não couber, splitta ele
     // Escreve dados na página
 }
 
 /*Insert some key on page*/
-btPage *searchPositionOnPageAndInsert(btPage *page, promotedKey *newKey) {
+BTPAGE *searchPositionOnPageAndInsert(BTPAGE *page, promotedKey *newKey) {
     // Encontra a posição para inserir a chave
     // Se não existir espaço, precisa criar uma nova página (usem uma função para criar)
     // Salvar dados da nova chave na página
@@ -83,7 +83,7 @@ btPage *searchPositionOnPageAndInsert(btPage *page, promotedKey *newKey) {
 /*
     If page size is odd the return is biggest slice
 */
-btPage *splitAndCreateNewNode(btPage **page) {
+BTPAGE *splitAndCreateNewNode(BTPAGE **page) {
     // Encontra a posição do meio das chaves
     // Cria espaço pra nova página
     // Copia metade das chaves pra página nova
@@ -93,21 +93,21 @@ btPage *splitAndCreateNewNode(btPage **page) {
 }
 
 /*Extract a promoted key from page*/
-promotedKey *extractpromotedKey(btPage *page) {
+promotedKey *extractpromotedKey(BTPAGE *page) {
     // Aloca espaço pra chave
     // Tira ela da página
     // Atualiza dados da página (filho, número de chaves, etc)
 }
 
 /*Split node and writes into file*/
-promotedKey *_split(btPage *page, FILE *fp, promotedKey *newKey) {
+promotedKey *_split(BTPAGE *page, FILE *fp, promotedKey *newKey) {
     // Divide a página, cria o novo nó (faça numa função auxiliar pois é complexo)
     // Extrai a chave promovida e atualiza os filhos da chave
     // Escreve a página nova e a que foi dividida (com suas atualizações) no arquivo
 }
 
 /*Used if promotions reaches root*/
-btPage *createNodeWithPromotedKey(promotedKey *promoKey) {
+BTPAGE *createNodeWithPromotedKey(promotedKey *promoKey) {
     // Se promoção cria estrutura para nova raiz,
     // Aloca espaço para ela
     // Salva dados da chave promovida na nova raiz
@@ -115,13 +115,13 @@ btPage *createNodeWithPromotedKey(promotedKey *promoKey) {
     // Pode reusar função que cria página nova e adicionar somente especificidades
 }
 
-Errors setNodeAsRoot(btPage *page, FILE *fp) {
+Errors setNodeAsRoot(BTPAGE *page, FILE *fp) {
     // Escreve página nova e atualiza o cabeçalho para conter ela como raiz
     // Deveria ser chamada junto com criação de novo nó quando promoção cria uma nova raiz
 }
 
 /*Recursive insertion*/
-promotedKey *_bTreeInsert(btPage *node, promotedKey *key, FILE *fp) {
+promotedKey *_bTreeInsert(BTPAGE *node, promotedKey *key, FILE *fp) {
     // Se nó a ser inserido a chave é folha, tenta inserir
     // Caso a inserção crie uma promoção, precisa retornar a chave promovida para a recursão
     // Se não for nó folha, procura qual sub-árvore seguir para inserir numa folha
@@ -132,7 +132,7 @@ promotedKey *_bTreeInsert(btPage *node, promotedKey *key, FILE *fp) {
     // Retornar chave promovida ou um valor NULL se não houve promoção
 }
 
-Errors bTreeInsert(PrimaryIndex *newRecord, btPage *root, FILE *fp) {
+Errors bTreeInsert(PrimaryIndex *newRecord, BTPAGE *root, FILE *fp) {
     // Função mais abstrata de inserção
     // Prepara os dados da nova chave
     // Tenta inserir recursivamente
@@ -141,9 +141,34 @@ Errors bTreeInsert(PrimaryIndex *newRecord, btPage *root, FILE *fp) {
 }
 
 /*Returns rrn if key exist else return -1*/
-long bTreeSelect(btPage *node, int key, FILE *fp) {
+long bTreeSelect(BTPAGE *node, int key, FILE *fp) {
     // Procura no nó atual se a chave existe
     // Se não existir, tenta procurar no filho adequado, recursivamente
     // Se encontrar a chave, retorna RRN dela
     // Se não encontrar (chegar num nó folha e não estiver lá), retorna -1
+}
+
+RECORD *createRecord(int key, long recordRRN){
+				RECORD* record = (RECORD *) calloc(1, sizeof(RECORD));
+				record->key = key;
+				record->recordRRN = recordRRN;
+				return record;
+}
+
+BTPAGE *createPage(RECORD* records, long* childs, boolean isLeaf, int numberOfKeys){
+				BTPAGE *page = (BTPAGE *) calloc(1, sizeof(BTPAGE));
+				page->records = records;
+				page->childs = childs;
+				page->isLeaf = isLeaf;
+				page->numberOfKeys = numberOfKeys;
+				return page;
+}
+
+PROMOTEDKEY *createPromoKey(int key, long recordRRN, long* childs){
+				PROMOTEDKEY *promo = (PROMOTEDKEY *) calloc(1, sizeof(PROMOTEDKEY));
+				promo->key = key;
+				promo->recordRRN = recordRRN;
+				promo->childs[0] = childs[0];
+				promo->childs[1] = childs[1];
+				return promo;
 }
