cdf5468df7176e7071d1492f208591a2ee5c2781
Ryan <ryansateles@gmail.com>
2022-06-21 21:22:24 -0300

    refactor: Main class it was renamed

--- Exercice4/code/BreadthFirstSearchRPG/src/main/java/Main.java
+++ Exercice4/code/BreadthFirstSearchRPG/src/main/java/Main.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main() {
         Scanner scanner = new Scanner(System.in);
+
         int vertexNumber = scanner.nextInt();
         for (int i = 0; i < vertexNumber; i++) {
             String questName = scanner.nextLine();
             String questDescription = scanner.nextLine();
         }
+
         int edgeNumber = scanner.nextInt();
-        for(int i = 0; i < edgeNumber; i++){
+        for (int i = 0; i < edgeNumber; i++) {
             int originId = scanner.nextInt();
             int destinyId = scanner.nextInt();
         }
+
         int originVertex = scanner.nextInt();
 
         //CHAMA GRAFO
