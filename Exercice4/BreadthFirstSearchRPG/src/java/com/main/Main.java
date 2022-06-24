e30d3b8ef926b1d83d4c1a57a2ad37ae59889925
Ryan <76923948+Ryrden@users.noreply.github.com>
2022-06-23 21:05:21 -0300

    Exercice 4 Done (#1)
    * basic structure and main class almost done
    
    * feature: Abstract graph package
    
    * refactor: Main class it was renamed
    
    * docs: package renamed
    
    * docs: moved main.class to a package
    
    * feature: Vertex now extends Answer
    
    new default pattern for the exercise and the next exercises
    
    * refactor: renamed ID variable to id
    
    * feature: Edge class created
    
    * feature: DigraphList class created
    
    * feature(Vertex.java): new method toString
    
    * Entrada e saida esperada do exercicio
    
    * docs&feature: change Project directory & Main done
    
    main: call BreadthFirstSearch tranverse algorithm
    docs: change Project directory only, code directory it was removed

---
+++ Exercice4/BreadthFirstSearchRPG/src/java/com/main/Main.java
@@ -0,0 +1,36 @@
+package com.main;
+
+import com.main.answer.Answer;
+import com.main.graph.BreadthFirstTraversal;
+import com.main.graph.DigraphList;
+import com.main.graph.Vertex;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int vertexNumber = scanner.nextInt();
+        scanner.nextLine();
+        ArrayList<Vertex> questLog = new ArrayList<>();
+        for (int i = 0; i < vertexNumber; i++) {
+            String questName = scanner.nextLine();
+            String questDescription = scanner.nextLine();
+            Answer questN = new Answer(i, questName, questDescription);
+            Vertex vertex = new Vertex(questN);
+            questLog.add(vertex);
+        }
+        DigraphList digraphList = new DigraphList(questLog);
+        int edgeNumber = scanner.nextInt();
+        for (int i = 0; i < edgeNumber; i++) {
+            int originId = scanner.nextInt();
+            int destinyId = scanner.nextInt();
+            digraphList.addEdge(questLog.get(originId), questLog.get(destinyId));
+        }
+        int originVertex = scanner.nextInt();
+        BreadthFirstTraversal breadthFirstTraversal = new BreadthFirstTraversal(digraphList);
+        breadthFirstTraversal.traverseGraph(questLog.get(originVertex));
+    }
+}
---
+++ Exercice4/BreadthFirstSearchRPG/src/java/com/main/graph/Vertex.java
@@ -0,0 +1,15 @@
+package com.main.graph;
+
+import com.main.answer.Answer;
+
+public class Vertex extends Answer {
+    protected Answer object;
+    public Vertex(Answer object) {
+        super(object.getId(), object.getName(), object.getDescription());
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
