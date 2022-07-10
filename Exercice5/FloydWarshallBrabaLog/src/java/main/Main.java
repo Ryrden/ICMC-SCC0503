672ec34765389ec568a83571e0160217eeb9d948
Ryan <ryansateles@gmail.com>
2022-07-09 23:38:04 -0300

    refactor: send methods from main to Floyd Class

--- Exercice5/FloydWarshallBrabaLog/src/java/main/Main.java
+++ Exercice5/FloydWarshallBrabaLog/src/java/main/Main.java
@@ -5,7 +5,6 @@ import main.graph.FloydWarshallTraversal;
 import main.graph.Vertex;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -39,85 +38,16 @@ public class Main {
 
             digraphMatrix.addEdge(pointOrigin, pointDestination,weight);
         }
-        Vertex originVertex = new Vertex(0, 0);
         FloydWarshallTraversal floydWarshallTraversal = new FloydWarshallTraversal(digraphMatrix);
-        floydWarshallTraversal.traverseGraph(originVertex);
+        floydWarshallTraversal.traverseGraph(null);
 
-        float[][] distanceMatrix = floydWarshallTraversal.getDistanceMatrix();
-        Vertex centralVertex = getCentralVertex(distanceMatrix, digraphMatrix);
-        Vertex peripheralVertex = getPeripheralVertex(distanceMatrix, digraphMatrix);
-        Vertex farthestVertexFromPeripheral = getFarthestVertexFromPeripheral(distanceMatrix,digraphMatrix, peripheralVertex);
+        Vertex centralVertex = floydWarshallTraversal.getCentralVertex();
+        Vertex peripheralVertex = floydWarshallTraversal.getPeripheralVertex();
+        Vertex farthestVertexFromPeripheral = floydWarshallTraversal.getFarthestVertexFromPeripheral();
 
         System.out.println(centralVertex);
         System.out.println(peripheralVertex);
         System.out.print(farthestVertexFromPeripheral);
-    }
-
-    private static Vertex getFarthestVertexFromPeripheral(float[][] distanceMatrix,DigraphMatrix digraphMatrix, Vertex peripheralVertex) {
-        int peripheralIndex = digraphMatrix.getVertices().indexOf(peripheralVertex);
-
-        Vertex farthestVertex = null;
-        int farthestVertexIndex = 0;
-        float maxDistance = Float.NEGATIVE_INFINITY;
-        for (int column = 0; column < distanceMatrix.length; column++) {
-            if (column == peripheralIndex) {
-                continue;
-            }
-            if (maxDistance < distanceMatrix[peripheralIndex][column]) {
-                maxDistance = distanceMatrix[peripheralIndex][column];
-                farthestVertexIndex = column;
-            }
-        }
-        farthestVertex = digraphMatrix.getVertices().get(farthestVertexIndex);
-        return farthestVertex;
-    }
-
-    private static Vertex getPeripheralVertex(float[][] distanceMatrix, DigraphMatrix digraphMatrix) {
-        Vertex peripheralVertex = null;
-        int peripheralVertexIndex = 0;
-        float[] maximumCostVertexes = getMaximumCostVertexes(distanceMatrix);
-
-        float max = Float.NEGATIVE_INFINITY;
-        for (int i = 0; i < maximumCostVertexes.length; i++) {
-            if (max < maximumCostVertexes[i]) {
-                max = maximumCostVertexes[i];
-                peripheralVertexIndex = i;
-            }
-        }
-
-        peripheralVertex = digraphMatrix.getVertices().get(peripheralVertexIndex);
-        return peripheralVertex;
-    }
-
-    private static Vertex getCentralVertex(float[][] distanceMatrix, DigraphMatrix digraphMatrix) {
-        Vertex centralVertex = null;
-        int centralVertexIndex = 0;
-        float[] maximumCostVertexes = getMaximumCostVertexes(distanceMatrix);
-        float min = Float.POSITIVE_INFINITY;
-        for (int i = 0; i < maximumCostVertexes.length; i++) {
-            if (min > maximumCostVertexes[i]) {
-                min = maximumCostVertexes[i];
-                centralVertexIndex = i;
-            }
-        }
-
-        centralVertex = digraphMatrix.getVertices().get(centralVertexIndex);
-        return centralVertex;
-    }
-
-    private static float[] getMaximumCostVertexes(float[][] distanceMatrix) {
-        float[] maximumCostVertexes = new float[distanceMatrix.length];
-
-        for (int column = 0; column < distanceMatrix[0].length; column++) {
-            float maxDistance = Float.NEGATIVE_INFINITY;
-            for (int row = 0; row < distanceMatrix.length; row++) {
-                if (distanceMatrix[row][column] > maxDistance) {
-                    maxDistance = distanceMatrix[row][column];
-                }
-            }
-            maximumCostVertexes[column] = maxDistance;
-        }
-        return maximumCostVertexes;
     }
 
 }
--- Exercice5/FloydWarshallBrabaLog/src/java/main/graph/FloydWarshallTraversal.java
+++ Exercice5/FloydWarshallBrabaLog/src/java/main/graph/FloydWarshallTraversal.java
@@ -21,6 +21,9 @@ public class FloydWarshallTraversal extends TraversalStrategy {
         distanceMatrix = new float[graph.getNumberOfVertices()][graph.getNumberOfVertices()];
     }
 
+    Vertex centralVertex;
+    Vertex peripheralVertex;
+
     @Override
     public void traverseGraph(Vertex source) {
         for (int i = 0; i < getGraph().getNumberOfVertices(); i++) {
@@ -59,5 +62,72 @@ public class FloydWarshallTraversal extends TraversalStrategy {
             distanceMatrixString.append('\n');
         }
         LOGGER.info(distanceMatrixString.toString());
+    }
+
+    public Vertex getFarthestVertexFromPeripheral() {
+        int peripheralIndex = getGraph().getVertices().indexOf(peripheralVertex);
+
+        Vertex farthestVertex = null;
+        int farthestVertexIndex = 0;
+        float maxDistance = Float.NEGATIVE_INFINITY;
+        for (int column = 0; column < distanceMatrix.length; column++) {
+            if (column == peripheralIndex) {
+                continue;
+            }
+            if (maxDistance < distanceMatrix[peripheralIndex][column]) {
+                maxDistance = distanceMatrix[peripheralIndex][column];
+                farthestVertexIndex = column;
+            }
+        }
+        farthestVertex = getGraph().getVertices().get(farthestVertexIndex);
+        return farthestVertex;
+    }
+
+    public Vertex getPeripheralVertex() {
+        Vertex peripheralVertex = null;
+        int peripheralVertexIndex = 0;
+        float[] maximumCostVertexes = getMaximumCostVertexes();
+
+        float max = Float.NEGATIVE_INFINITY;
+        for (int i = 0; i < maximumCostVertexes.length; i++) {
+            if (max < maximumCostVertexes[i]) {
+                max = maximumCostVertexes[i];
+                peripheralVertexIndex = i;
+            }
+        }
+
+        peripheralVertex = getGraph().getVertices().get(peripheralVertexIndex);
+        return peripheralVertex;
+    }
+
+    public Vertex getCentralVertex() {
+        Vertex centralVertex = null;
+        int centralVertexIndex = 0;
+        float[] maximumCostVertexes = getMaximumCostVertexes();
+        float min = Float.POSITIVE_INFINITY;
+        for (int i = 0; i < maximumCostVertexes.length; i++) {
+            if (min > maximumCostVertexes[i]) {
+                min = maximumCostVertexes[i];
+                centralVertexIndex = i;
+            }
+        }
+
+        centralVertex = getGraph().getVertices().get(centralVertexIndex);
+        return centralVertex;
+    }
+
+    private float[] getMaximumCostVertexes() {
+        float[] maximumCostVertexes = new float[distanceMatrix.length];
+
+        for (int column = 0; column < distanceMatrix[0].length; column++) {
+            float maxDistance = Float.NEGATIVE_INFINITY;
+            for (int row = 0; row < distanceMatrix.length; row++) {
+                if (distanceMatrix[row][column] > maxDistance) {
+                    maxDistance = distanceMatrix[row][column];
+                }
+            }
+            maximumCostVertexes[column] = maxDistance;
+        }
+        return maximumCostVertexes;
     }
 }
