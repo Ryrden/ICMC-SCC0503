7a44dbdeacc10b8b7c00e12994bdbcfe96e8dfd4
Ryan <ryansateles@gmail.com>
2022-07-09 22:15:04 -0300

    feature: euclidianDistance method implemented

--- Exercice5/FloydWarshallBrabaLog/src/java/main/graph/Vertex.java
+++ Exercice5/FloydWarshallBrabaLog/src/java/main/graph/Vertex.java
@@ -2,9 +2,14 @@ package main.graph;
 
 public record Vertex(int x, int y) {
 
+    public float euclideanDistance(Vertex other) {
+        return (float) Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
+    }
+
     public String getName() {
         return "(" + x + "," + y + ")";
     }
+
     public String toString() {
         return x + "," + y;
     }
