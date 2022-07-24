4f6477a819e7e98a20fdb824866fe90562d610c4
Ryan <ryansateles@gmail.com>
2022-07-24 13:24:57 -0300

    docs: basic structure

---
+++ Exercice7/DelaunayTriangulationTheInternship/src/java/main/graph/GraphInterface.java
@@ -0,0 +1,30 @@
+package main.graph;
+
+public interface GraphInterface
+{
+    void addVertex(Vertex vertex);
+    void removeVertex(Vertex vertex);
+
+    void addEdge(Vertex source, Vertex destination);
+    void addEdge(Vertex source, Vertex destination, float weight);
+    void removeEdge(Vertex source, Vertex destination);
+
+    boolean edgeExists(Vertex source, Vertex destination);
+
+    boolean hasAnyEdge(Vertex vertex);
+
+    int getFirstConnectedVertexIndex(Vertex vertex);
+    int getNextConnectedVertexIndex(Vertex vertex, int currentEdge);
+
+    String toString();
+
+    void printInGraphviz(String fileName);
+
+    float getDistance(Vertex source, Vertex destination);
+
+    Vertex getFirstConnectedVertex(Vertex vertex);
+
+    Vertex getNextConnectedVertex(Vertex source, Vertex currentConnection);
+
+    void lockEdge(Vertex source, Vertex destination, int lockID);
+}
